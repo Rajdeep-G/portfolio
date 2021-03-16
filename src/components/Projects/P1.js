b8af47166367eb49aeaf7f6e48ad61d0b5f87f9f
Rajdeep <ghoshrajdeep2000@gmail.com>
2021-03-17 00:23:24 +0530

    project started

--- src/components/Projects/P1.js
+++ src/components/Projects/P1.js
@@ -1,21 +1,38 @@
+import GitHubButton from 'react-github-btn'
+import Fade from 'react-reveal/Fade';
 function P1() {
     return (
         <div>
-            <section class="text-gray-600 body-font">
-                <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-                    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-                        <img class="object-cover object-center rounded" alt="hero" src={process.env.PUBLIC_URL + "/Assets/R.png"}>
-                        </img></div>
-                    <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-                        <div class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
-                        readymade gluten
-      </div>
-                        <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
-                        <div class="flex justify-center">
-                            <button class="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">Button</button>
-                            <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
+
+            <section class="text-blue-800 body-font  ">
+                <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center  ">
+                    <Fade left>
+                        <div class="lg:max-w-lg lg:w-full md:w-1/3 w-5/6 mb-10 md:mb-0  ">
+                            <img class="shadow-2xl object-cover object-center rounded" alt="project-1" src={process.env.PUBLIC_URL + "/Assets/projects/1.PNG"} />
                         </div>
+                    </Fade>
+
+                    <div class="lg:flex-grow md:w-1/2 lg:pl-32 mt-8 md:mt-0 md:pl-64 flex flex-col md:items-start md:text-left items-center text-center">
+                        <div class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-800">
+                            Cf bot (for Discord)
+                            </div>
+                        <p class="mb-4 leading-relaxed text-2xl">Discord bot to provide quick happenings in Codeforces</p>
+
+                        <div class="flex justify-center">
+                            <div class="mr-4 shadow-xl inline-flex text-gray-800 border border-gray-800 py-1 px-4 rounded text-lg cursor-default">Discord.js</div>
+                            <div class="mr-4 inline-flex shadow-xl text-gray-800 border border-gray-800 py-1 px-4 rounded text-lg cursor-default">Javascript</div>
+                            <div class="inline-flex text-gray-800 shadow-xl border border-gray-800 py-1 px-4 rounded text-lg cursor-default">Heroku</div>
+
+
+                        </div>
+
+                        <div className="mt-4">
+                            <GitHubButton href="https://github.com/Rajdeep-G/Cf-bot" data-color-scheme="no-preference: light; light: light; dark: dark;" data-icon="" data-size="large" data-show-count="false" aria-label="Star Rajdeep-G/Cf-bot on GitHub">View Source Code</GitHubButton>
+                        </div>
+
+
                     </div>
+
                 </div>
             </section>
         </div>
